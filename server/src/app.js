4266c58bbee57ea481fc1f8074ff9804fa46c6e3
Carlos Diaz <carlos.diaz@adagetech.com>
2024-08-04 19:15:08 -0500

    subir a produccion

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from"morgan";
 import router from "./routes/producto.routes.js";
+import routeUser from "./routes/user.routes.js"
 import bodyParser from "body-parser";
 import cors from 'cors';
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json({ limit: '10mb' }));
 app.use("/api",router)
+app.use("/api",routeUser)
 app.use(bodyParser.urlencoded({extended: true}))
 
 // app.use(express.json());
